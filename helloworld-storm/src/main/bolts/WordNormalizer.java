882038f649d7e5e25a799221027c83f5e730ff35
xxdeyy <xxdeyy@gmail.com>
2016-03-01 15:14:18 +0800

    adf

---
+++ helloworld-storm/src/main/bolts/WordNormalizer.java
@@ -0,0 +1,57 @@
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class WordNormalizer implements IRichBolt {
+
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.collector = outputCollector;
+
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String sentence = tuple.getString(0);
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            word = word.trim();
+            if (!word.isEmpty()) {
+                word = word.toLowerCase();
+                //发布这个单词
+                List<Tuple> a = new ArrayList<>();
+                a.add(tuple);
+                collector.emit(a, new Values(word));
+            }
+        }
+        //对元组做出应答
+        collector.ack(tuple);
+
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("word"));
+
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+}
---
+++ helloworld-storm/src/main/java/TopologyMain.java
@@ -0,0 +1,27 @@
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+
+public class TopologyMain {
+    public static void main(String[] args) throws InterruptedException {
+        //定义拓扑
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("word-reader", new WordReader());
+        builder.setBolt("word-normalizer", new WordNormalizer()).shuffleGrouping("word-reader");
+        builder.setBolt("word-counter", new WordCounter(), 2).fieldsGrouping("word-normalizer", new Fields("word"));
+
+        //配置
+        Config conf = new Config();
+        conf.put("wordsFile", "src/main/resources/words.txt");
+        conf.setDebug(false);
+
+        //运行拓扑
+        conf.put(Config.TOPOLOGY_MAX_SPOUT_PENDING, 1);
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("Getting-Started-Topologie", conf, builder.createTopology());
+        Thread.sleep(1000);
+        cluster.shutdown();
+    }
+
+}
