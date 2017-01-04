06056f618357c957debfe5cbd25a807faced0dd1
xxdeyy <xxdeyy@gmail.com>
2017-01-04 22:48:41 +0800

    没啥特别的

---
+++ helloworld-kafka/src/main/java/TestProducer.java
@@ -0,0 +1,31 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class TestProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            producer.send(new ProducerRecord<>("test", "key" + i, "value" + i), (metadata, e) -> {
+                if (e != null) {
+                    e.printStackTrace();
+                }
+                System.out.println("The offset of the record we just sent is: " + metadata.offset());
+            });
+        }
+
+        producer.close();
+    }
+}
